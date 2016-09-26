c7b364152cb1b6b87576641dd795c397805ad3ed
kouhot <kouhot6210@gmail.com>
2016-09-26 21:26:24 +0900

    UserモデルとMeetingモデルでの関連付け

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,4 @@
 class Meeting < ActiveRecord::Base
-  belongs_to :promoter
-  belongs_to :participant
+  belongs_to :promoter, class_name: "User", foreign_key: promoter_id
+  belongs_to :participant, class_name: "User", foreign_key: participant_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_one :profile
+  has_many :user_promoters, class_name: "Meeting", foreign_key: :promoter_id
+  has_many :promoters, through: :user_promoters
+  has_many :user_participants, class_name: "Meeting", foreign_key: :participant_id
+  has_many :participants, through: :user_participants
 end
