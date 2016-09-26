8400f9c4225f8012855850ecfa58695733c770e2
kouhot <kouhot6210@gmail.com>
2016-09-26 18:05:37 +0900

    Profilesコントローラを作成して、プロフィールを作成、変更できるように

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#mypage'
 
+  resources :users, except: %w(index) do
+    resources :profiles, only: %w(new show)
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
